94a44c974737746d51d8c17a02ec8172fa43ea62
Misael Borges <ml_bbd@hotmail.com>
2025-04-25 11:22:13 -0300

    Iniciando Menu

--- src/Components/HeaderMobile/index.jsx
+++ src/Components/HeaderMobile/index.jsx
@@ -5,29 +5,31 @@ import { IoIosMore } from "react-icons/io";
 
 const Header = () => {
   return (
-    <header>
-      <img src={Logo} alt="Logo do projeto CrerSer" />
-      <nav>
-        <ul>
-          <li>
+    <header className="header-mobile">
+      <div className="logo-header">
+        <img src={Logo} alt="Logo do projeto CrerSer" />
+      </div>
+      <nav className="nav-headerMobile">
+        <ul className="nav-list-headerMobile">
+          <li className="nav-item-headerMobile">
             <a href="#">
               <FaHome />
               <span>Link 1</span>
             </a>
           </li>
-          <li>
+          <li className="nav-item-headerMobile">
             <a href="#">
               <FaInfoCircle />
               <span>Link 2</span>
             </a>
           </li>
-          <li>
+          <li className="nav-item-headerMobile">
             <a href="#">
               <FaShoppingCart />
               <span>Link 3</span>
             </a>
           </li>
-          <li>
+          <li className="nav-item-headerMobile">
             <a href="#">
               <IoIosMore />
               <span>Mais</span>
